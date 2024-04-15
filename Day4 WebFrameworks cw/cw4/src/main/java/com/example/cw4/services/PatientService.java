bda3fa076f5cb82cf81f8a86b94aa4977664a642
Dhisha-D <124854764+Dhisha-D@users.noreply.github.com>
2024-04-15 14:16:36 +0530

    Day4cw

---
+++ Day4 WebFrameworks cw/cw4/src/main/java/com/example/cw4/services/PatientService.java
@@ -0,0 +1,40 @@
+package com.example.cw4.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.stereotype.Service;
+
+import com.example.cw4.model.Patient;
+import com.example.cw4.repository.PatientRepo;
+
+@Service
+public class PatientService {
+    public PatientRepo patientRepo;
+    public PatientService(PatientRepo patientRepo)
+    {
+        this.patientRepo = patientRepo;
+    }
+    public boolean savePatient(Patient patient)
+    {
+        try{
+            patientRepo.save(patient);
+
+        }
+        catch(Exception e)
+        {
+            return false;
+        }
+        return true;
+    }
+    public List<Patient> getPatients()
+    {
+        return patientRepo.findAll();
+    }
+    public Patient getPatientById(int id)
+    {
+        Optional<Patient> obj = patientRepo.findById(id);
+        return obj.orElse(null);
+    }
+}
+
